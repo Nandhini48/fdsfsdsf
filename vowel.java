c9010cb0dc2e9c7b451926e016b8160e4af29a67
Nandhini48 <nandhininandhuas@gmail.com>
2016-07-16 04:33:02 -0700

    Add files via upload

---
+++ vowel.java
@@ -0,0 +1,20 @@
+package vowels;
+import java.util.Scanner;
+public class Vowels{
+    public static void main(String[] args) {
+        char ch;
+        System.out.println("enter the char");
+        Scanner v=new Scanner(System.in);
+         ch=v.next().charAt(0);
+
+			if(ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || 
+			ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U')
+		
+		System.out.println("Vowels : " );
+                else
+                            System.out.println("consonant");
+                         
+                }}
+  
+    
+
